0af4cdf5ca076ec7fa6d721aa0fec31170869fa7
whiterasbk <whiterasbk@qq.com>
2024-03-21 22:01:50 +0800

    add ONE OK ROCK LISTEN

--- src/test/kotlin/mider.kt
+++ src/test/kotlin/mider.kt
@@ -18,7 +18,97 @@ import javax.sound.midi.MidiSystem
 import javax.sound.sampled.AudioSystem
 import kotlin.concurrent.timerTask
 
+
 fun main() {
+    `ONE OK ROCK LISTEN`()
+}
+
+fun `ONE OK ROCK LISTEN`() {
+    val main: MiderDSL.() -> Unit =  {
+        bpm = 200
+        velocity = 108
+        "#C" {
+            +"e"
+            +"gee+ gee+ cded&d+ o" // u always call me full of regret
+            +"c cdeC+ ba e&e++ o++" // u want me to save you again
+            +"g-a-g-a- C-a-C g-a-g-a- C-a-C"
+            +"C. D-&D EE+ G+"
+            +"g-a-g-a- C-a-C g-a-g-a- C-a-C"
+            +"C. D-&D ED+ b+"
+            +"EEE.D- EEE+" // all these years the days go by
+            +"EEE.E- E-D- DD+" // ive seen u fall a million times
+            +"EEC↑ ~+ AG&G++" // everybody makes mistakes
+            +"o+oo."
+            +"D- EEE.D- EEE+" // it feels so hard to watch u hurt
+            +"EEE.E- ED D.C-" // from that pain a lesson learned
+            +"EEC↑ ~+ A.G-&G++" // this is how u find ur way
+            +"o++o"
+            +"g-a-g-a- C-a-C g-a-g-a- C-a-C"
+            +"C. D-&D EE+ G+"
+            +"g-a-g-a- C-a-C g-a-g-a- C-a-C"
+            +"C. D-&D ED+ b+"
+            +"EEE.D- EEE+" // u feel so lost ive been there 2
+            +"EEE.E- E-D- DD+" // skies so derk no way through
+            +"EEC↑ ~+ A.G-&G++" // stories only scars can tell
+            +"o+oo-"
+            +"C-C EEE.D- EEE+" // ive got so much love 4 u my friend
+            +"EEE.E- ED D.C-" // rider or die until the end
+            +"EEC↑ ~+ A.G-&G++&G" // but only u can save yourself
+            +"D E D C" // you just have to
+            +"ED-E-EED-E.&E o-" // listen, listen
+            +"C- C D E D&D oo+" // i hope that u know
+            +"ED-E-EED-E.&E o-" // listen, listen
+            +"C- C D E D oo-o+" // i wont let u go
+            +"C- CDE C↑&~ BAG CDE D&D+ o." // i wish i could save u from the pain uve been through
+            +"C- CDE C↑&~ BAG CDE D&D oo" // and all i can tell u is the best thing to do
+            +"C DE+ DE++"  // u gotta listen, listen
+            +"G.A- G.A- G.A- G-F-E"
+            +"DE+ DE++" // listen, listen
+            +"G.A- G.A- G.A- G-F-E"
+            +"DE+ DE++" // listen, listen
+            +"G.A- G.A- G.A- G-F-E C+.o"
+            +"C-D-C-D-E-D-E" // to ur heart
+            +"G.A- G.A- G.A- G-F-E"
+            +"DE+ DD-E.&E+" // listen, listen
+            +"EEG+&G Cb+"
+            +"DE+ DE.D- C+" // listen, listen
+            +"CEG+&G gC+"
+            +"DE+DE+. o-" // listen, listen
+            +"C- CDE D&D+ o+" // 信じよう
+            +"DE+DE+.&E-C-" // listen, listen
+            +"CDE D:G C↑ C↑ A C↑:C  CDE C↑ ~ BAG CDE D" // 降り止まない雨などない
+            +"C↑C↑BC↑"
+            +"CDE C↑ ~ BAG CDE D+" // and all i can tell u is the best thing to do
+            +"C↑D↑C↑" // u gotta
+            +"DE+DE++" // listen, listen
+            +"G.A- G.A- G.A- G-F-E"
+            +"C+.EC++"
+            +"E.F- E.F- E D↑~~"
+            +"DE+DE++" // listen, listen
+            +"G.A- G.A- G.A- G-F-E"
+            +"g-a-g-a- C-a-C g-a-g-a- C-a-C"
+            +"G.A- G.A- G.A- G-F-E"
+        }
+        track {
+            velocity = 80
+            "#C" {
+                +"o"
+                +"A2 E3 A3+ F2 C3 F3+ C3 G3 C4+ G2 D3 G3+"
+                +"A2. E3- A3+  F2 C3 F3+ C3 G3 C4+ G2 D3 G3+"
+
+                repeat(30) {
+                    +"A2 E3 A3 E3"
+                    +"F2 C3 F3 C3"
+                    +"C3 G3 C4 G3"
+                    +"G2 D3 G3 D3"
+                }
+            }
+        }
+    }
+    play(block = main)
+}
+
+fun `HAPPY BIRTHDAY`() {
     val dsl: MiderDSL.() -> Unit = {
         bpm = (bpm * 1.5).toInt()
         program = MidiInstrument.recorder
